868759dd121b12bc0b6eb29e5f5813c916a42023
nathanallen <thenathanator@gmail.com>
2016-09-29 13:25:50 -0700

    cleanup

--- sanity-check.js
+++ sanity-check.js
@@ -1,29 +1,47 @@
 var sanityCheck = (function(){
   var self = {
-    errors: []
+    errors: [],
+    display: {
+      tagname: "div",
+      style: "position:fixed; top:0; left:20px; right:20px; background:white;",
+      message: "There is an error in your javascript. Please open your Developer Console for the complete error message."
+    }
   };
 
-  window.onerror = function(message, source, lineno, colno, error) {
+  window.onerror = recordError;
+  window.onload = checkForErrors;
+
+  return sanityCheck;
+
+  ////
+
+  function recordError(message, source, lineno, colno, error) {
     self.errors.push(message);
   };
 
-  window.onload = function() {
+  function checkForErrors() {
     if (self.errors.length) {
-      var el = document.createElement("div");
-      el.innerHTML = (
-        "<h1 style='position:fixed; top:0; left:20px; right:20px; background:white;'>" +
-          "plz to check your console for an important error message" +
-          "<br><br>" +
-          "<ol><li>" + self.errors.join("<li>") + "<ol>" +
-        "</h1>"
-      );
-      document.body.appendChild(el);
+      displayErrors();
     }
   }
 
-  return function sanityCheck(scope) {
+  function displayErrors() {
+    var el = document.createElement(self.display.tagname);
+    el.innerHTML = (
+      "<h1 style='" + self.display.style + "'>" +
+        self.display.message +
+        "<br><br>" +
+        "<ol><li>" + self.errors.join("<li>") + "<ol>" +
+      "</h1>"
+    );
+    document.body.appendChild(el);
+  }
+
+  // TODO: speculative script-level functionality
+  function sanityCheck(scope) {
     var file = scope.src;
     console.log("sanityCheck:", file);
   }
+
 
 }());
